f0da5222b7cb5aa2c9ffd79bd7336364b8a16df4
Junior Cintra <junior.developer@outlook.com.br>
2022-01-04 00:08:02 -0300

    incluindo connect multiparty

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 var express = require("express"),
   bodyParser = require("body-parser"),
+  multiparty = require("connect-multiparty"),
   mongodb = require("mongodb"),
   objectId = require("mongodb").ObjectId;
 
@@ -8,6 +9,7 @@ var app = express();
 //body-parser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(multiparty());
 
 var port = 8080;
 
