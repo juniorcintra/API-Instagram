ebefa3dee50891af2530e86891b83529ad3832e6
Junior Cintra <junior.developer@outlook.com.br>
2022-01-03 22:28:37 -0300

    method put atualizando postagens

--- server.js
+++ server.js
@@ -70,3 +70,23 @@ app.get("/api/:id", function (req, res) {
     });
   });
 });
+
+app.put("/api/:id", function (req, res) {
+  db.open(function (err, mongoclient) {
+    mongoclient.collection("postagens", function (err, collection) {
+      collection.update(
+        { _id: objectId(req.params.id) },
+        { $set: { titulo: req.body.titulo } },
+        {},
+        function (err, records) {
+          if (err) {
+            res.json(err);
+          } else {
+            res.json(records);
+          }
+          mongoclient.close();
+        }
+      );
+    });
+  });
+});
