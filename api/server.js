464fd7affd2748a0ef186559b13fb0f13fc9664e
Junior Cintra <junior.developer@outlook.com.br>
2022-01-04 00:22:32 -0300

    subindo imagens pelo formulario

--- api/server.js
+++ api/server.js
@@ -2,7 +2,8 @@ var express = require("express"),
   bodyParser = require("body-parser"),
   multiparty = require("connect-multiparty"),
   mongodb = require("mongodb"),
-  objectId = require("mongodb").ObjectId;
+  objectId = require("mongodb").ObjectId,
+  fs = require("fs");
 
 var app = express();
 
@@ -31,17 +32,32 @@ app.get("/", function (req, res) {
 app.post("/api", function (req, res) {
   res.setHeader("Access-Control-Origin", "*");
 
-  var dados = req.body;
+  var date = new Date();
 
-  db.open(function (err, mongoclient) {
-    mongoclient.collection("postagens", function (err, collection) {
-      collection.insert(dados, function (err, records) {
-        if (err) {
-          res.json({ status: "erro" });
-        } else {
-          res.json({ status: "inclusao realizada com sucesso" });
-        }
-        mongoclient.close();
+  var time_stamp = date.getTime();
+
+  var url_imagem = time_stamp + "_" + req.files.arquivo.originalFilename;
+
+  var path_origem = req.files.arquivo.path;
+  var path_destino = "./uploads/" + url_imagem;
+
+  fs.rename(path_origem, path_destino, function (err) {
+    if (err) {
+      res.status(500).json({ error: err });
+      return;
+    }
+
+    var dados = { url_imagem: url_imagem, titulo: req.body.titulo };
+    db.open(function (err, mongoclient) {
+      mongoclient.collection("postagens", function (err, collection) {
+        collection.insert(dados, function (err, records) {
+          if (err) {
+            res.json({ status: "erro" });
+          } else {
+            res.json({ status: "inclusao realizada com sucesso" });
+          }
+          mongoclient.close();
+        });
       });
     });
   });
